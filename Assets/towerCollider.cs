c6d797e66174e9955548340a840ea1f068eef0a1
Kevin <kjeong0@gmail.com>
2014-11-20 21:31:45 -0500

    i don't remember what i changed

---
+++ Assets/towerCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerCollider : MonoBehaviour {
+	public GameObject door;
+	public GameObject sounds;
+	Animator animator;
+
+	void OnTriggerEnter (Collider other){
+		animator = door.GetComponent<Animator>();
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+
+		if (!(state.nameHash == Animator.StringToHash("Base Layer.Close"))){
+			Debug.Log ("close");
+			animator.SetTrigger (Animator.StringToHash("CloseMe"));
+		}
+		if (!sounds.activeSelf){
+			sounds.SetActive(true);
+		}
+	}
+}
